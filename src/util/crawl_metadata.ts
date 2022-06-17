1586a16f7878c66f0ad2d3c4e93bd538926eb846
ParkJiHo <iks15174@daum.net>
2022-06-17 16:54:27 +0900

    feat: make crawler that get web page metadata

---
+++ src/util/crawl_metadata.ts
@@ -0,0 +1,31 @@
+import metascraper from 'metascraper';
+import axios from 'axios';
+import metascraperUrl from 'metascraper-url';
+import metascraperTitle from 'metascraper-title';
+import metascraperImage from 'metascraper-image';
+import metascraperDate from 'metascraper-date';
+import metascraperDescription from 'metascraper-description';
+import metascraperPublisher from 'metascraper-publisher';
+import { MetadataRes } from '../interfaces';
+const aixosInstance = axios.create();
+
+export const crawlMetadata = async (url: string) => {
+  const response = await aixosInstance.get(url);
+  const html = await response.data;
+  const metadata = await metascraper([
+    metascraperUrl(),
+    metascraperTitle(),
+    metascraperImage(),
+    metascraperDate(),
+    metascraperDescription(),
+    metascraperPublisher(),
+  ])({ html, url });
+  return {
+    date: new Date(metadata.date).toISOString(),
+    description: metadata.description,
+    image: metadata.image,
+    publisher: metadata.publisher,
+    title: metadata.title,
+    url: url,
+  } as MetadataRes;
+};
